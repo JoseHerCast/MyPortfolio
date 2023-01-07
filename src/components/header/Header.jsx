6f383a87d03dda97c2c8651e6da55bb546f4c9e2
JoseHerCast <carrousel.182@hotmail.com>
2023-01-07 02:48:34 -0600

    setup github pages

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
             <div className="container header_container">
                 <h5>Hola soy</h5>
                 <h1>José Hernández</h1>
-                <h5 className="text-light">Desarrollador Web/Móvil</h5>
+                <h5 className="text-light">Desarrollador Full-stack</h5>
                 <CTA></CTA>
                 <HeaderSocials></HeaderSocials>
                 <div className="me">
